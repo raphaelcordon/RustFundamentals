a8f92def59c394561d72fedc35721febb3c6a6a6
Raphael Cordon <raphael.cordon@sefit.ch>
2026-03-09 21:01:22 +0100

    Clean repository and apply gitignore

--- 02-FunctionBasics/02-TheBorrowingConcept/target/debug/deps/RustDukeFundamentals-7ba5c1af1563a521.d
+++
@@ -1,5 +0,0 @@
-/Users/raphaelcordon/Drive/Studies/Rust/Coursera/RustDukeFundamentals/target/debug/deps/RustDukeFundamentals-7ba5c1af1563a521.d: src/main.rs
-
-/Users/raphaelcordon/Drive/Studies/Rust/Coursera/RustDukeFundamentals/target/debug/deps/RustDukeFundamentals-7ba5c1af1563a521: src/main.rs
-
-src/main.rs:
--- 02-FunctionBasics/02-TheBorrowingConcept/target/debug/deps/log-83f68ae8364606ba.d
+++
@@ -1,8 +0,0 @@
-/Users/raphaelcordon/Drive/Studies/Rust/Coursera/RustDukeFundamentals/target/debug/deps/log-83f68ae8364606ba.d: /Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/lib.rs /Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/macros.rs /Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/serde.rs /Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/__private_api.rs
-
-/Users/raphaelcordon/Drive/Studies/Rust/Coursera/RustDukeFundamentals/target/debug/deps/liblog-83f68ae8364606ba.rmeta: /Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/lib.rs /Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/macros.rs /Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/serde.rs /Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/__private_api.rs
-
-/Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/lib.rs:
-/Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/macros.rs:
-/Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/serde.rs:
-/Users/raphaelcordon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/__private_api.rs:
